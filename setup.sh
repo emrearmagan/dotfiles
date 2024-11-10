9975b17c050897a174a6c8cc21fb38db85c4155f
Emre Armagan <emrearmagan.dev@gmail.com>
2024-11-10 19:44:38 +0100

    configure iTerm in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env zsh
 ############################
 # This script creates symlinks from the home directory to any desired dotfiles in $HOME/dotfiles
-# And also installs MacOS Software
 # And also installs Homebrew Packages and Casks (Apps)
-# And also sets up VS Code
-# And also sets up Sublime Text
+# And also sets up iTerm
 ############################
 
 dotfiledir="$(pwd)"
 echo dotfiledir
 # list of files/folders to symlink in ${homedir}
-files=(.zshrc .zprofile system/.alias system/.alias.macos system/.functions homebrew/Brewfile)
+files=(
+    zsh/.zshrc
+    zsh/.zprofile
+    system/.alias
+    system/.alias.macos
+    system/.functions
+    homebrew/Brewfile
+)
 
 # change to the dotfiles directory
 echo "Changing to the ${dotfiledir} directory"
@@ -25,13 +30,24 @@ for file in "${files[@]}"; do
     fi
 done
 
-# Run the MacOS Script
-#./macOS.sh#
+# Set up iTerm2 preferences if it is installed
+if [ -d "/Applications/iTerm.app" ]; then
+  echo "Setting up iTerm2 preferences..."
+
+  # Specify the preferences directory
+  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${dotfiledir}/iterm"
+
+  # Tell iTerm2 to use the custom preferences in the directory
+  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+  echo "iTerm Complete!"
+else
+  echo "Error: iTerm2 is not installed. Skipping iTerm2 setup."
+fi
+
 
 # Run the Homebrew Script
 sh ./homebrew/brew.sh
 
-# Run the Sublime Script
-#./sublime.sh
 
 echo "Installation Complete!"
