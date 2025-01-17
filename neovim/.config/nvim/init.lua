0f0e922d8b2c3bc19fb96fc3c5d108ab3c75d534
Emre Armagan <emrearmagan.dev@gmail.com>
2025-01-18 00:04:43 +0100

    update keybindings

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,28 +1,31 @@
 require("config.lazy")
 
+-- Basic Options
 vim.opt.number = true
 vim.opt.relativenumber = true
-
-vim.opt.guicursor = "n-v-c:ver25,i:ver25,r-cr:ver25"
+vim.opt.tabstop = 2       -- Number of spaces a tab counts for
+vim.opt.softtabstop = 2   -- Number of spaces per Tab when editing
+vim.opt.shiftwidth = 2    -- Number of spaces for indentation
+vim.opt.expandtab = true  -- Convert tabs to spaces
 
 -------------------- Keybindings ------------------------
 
---- to open Neotree press space + e
-vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { silent = true })
+local wk = require("which-key")
+local conform = require("conform")
 
---- Telescope key maps
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>tg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>tb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = 'Telescope help tags' })
+wk.add({
+    ---- Neotree
+    { "<leader>n", group = "Files" },
+    { "<leader>ne", ":Neotree toggle<CR>", desc = "File tree" },
 
+    ---- Fzf
+    { "<leader>f", group = "Search"},
+    { "<leader>ff", "<cmd>FzfLua files<CR>", desc = "Find files" },
+    { "<leader>fg", "<cmd>FzfLua live_grep<CR>", desc = "Live grep" },
+    { "<leader>fb", "<cmd>FzfLua buffer<CR>", desc = "Find buffer" },
+    { "<leader>ft", "<cmd>FzfLua help_tag<CR>", desc = "Find help tags"},
 
-    -- Keybindings for fzf-lua
-vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", { desc = "Find files" })
-vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", { desc = "Live grep" })
-vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", { desc = "Find buffers" })
-vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua help_tags<CR>", { desc = "Find help tags" })
-
---- show notify history
---- vim.api.nvim_set_keymap("n", "<leader>nh", ":lua require('notify').history()<CR>", { noremap = true, silent = true })
+    ---- Formatting
+    { "<leader>i", group = "Format"},
+    { "<leader>ii", function() require("conform").format({ async = true, lsp_fallback = true }) end, desc = "Format Buffer" },
+})
