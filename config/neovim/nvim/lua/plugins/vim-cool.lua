2b6c0e07a8907fc7002c9ec90573f1129945b818
Emre Armagan <emrearmagan.dev@gmail.com>
2025-10-13 23:35:53 +0200

    add vim cool plugin

---
+++ config/neovim/nvim/lua/plugins/vim-cool.lua
@@ -0,0 +1,6 @@
+-- Automatically removes search highlighting when you're done searching,
+-- similar to running `:noh` (nohlsearch) manually.
+return {
+	"romainl/vim-cool",
+	config = function() end,
+}
