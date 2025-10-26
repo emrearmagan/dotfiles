2753b66a68c821278232a0f46f60c69b70090f41
Emre Armagan <emrearmagan.dev@gmail.com>
2025-10-26 15:01:45 +0100

    update config

--- config/sketchybar/plugins/brew.sh
+++ config/sketchybar/plugins/brew.sh
@@ -2,60 +2,32 @@
 
 source "$HOME/.config/sketchybar/colors.sh"
 
-popup() {
-  sketchybar --set $NAME popup.drawing=$1
-}
+# COUNT=$(zsh -l -c "/opt/homebrew/bin/brew outdated --quiet" | wc -l | tr -d ' ')
+COUNT=$(zsh -l -c "brew outdated --quiet" | wc -l | tr -d ' ')
+COLOR=$RED
 
-update() {
-  COUNT=$(zsh -l -c "/opt/homebrew/bin/brew outdated --quiet" | wc -l | tr -d ' ')
-  COLOR=$RED
-  case "$COUNT" in
-    0)
-      COLOR=$GREEN
-      COUNT=􀆅
-      ;;
-    [12])
-      COLOR=$WHITE
-      ;;
-    [34])
-      COLOR=$YELLOW
-      ;;
-    [5-7])
-      COLOR=$PEACH
-      ;;
-    *)
-      COLOR=$RED
-      ;;
-  esac
-  sketchybar --set "$NAME" label="$COUNT" icon.color=$COLOR
-}
-
-case "$SENDER" in
-  "routine"|"forced")
-    update
-    ;;
-  "mouse.entered")
-    # Remove previous popup items
-    sketchybar --remove '/brew.popup.*/'
-    # Get outdated packages
-    OUTDATED=$(zsh -l -c "/opt/homebrew/bin/brew outdated --quiet")
-    if [ -z "$OUTDATED" ]; then
-      OUTDATED="No outdated packages!"
-    fi
-    COUNTER=0
-    while read -r pkg; do
-      COUNTER=$((COUNTER+1))
-      popup_item=(
-        label="$pkg"
-        drawing=on
-        position=popup.brew
-      )
-      sketchybar --clone brew.popup.$COUNTER brew --set brew.popup.$COUNTER "${popup_item[@]}"
-    done <<<"$OUTDATED"
-    popup on
-    ;;
-  "mouse.exited"|"mouse.exited.global")
-    popup off
-    sketchybar --remove '/brew.popup.*/'
-    ;;
+case "$COUNT" in
+0)
+	COLOR=$GREEN
+	COUNT=􀆅
+	;;
+[12])
+	COLOR=$WHITE
+	;;
+[34])
+	COLOR=$YELLOW
+	;;
+[5-7])
+	COLOR=$PEACH
+	;;
+*)
+	COLOR=$RED
+	;;
 esac
+sketchybar --set "$NAME" label="$COUNT" icon.color=$COLOR
+
+# Handle mouse click to trigger brew_update event
+if [ "$SENDER" = "mouse.clicked" ]; then
+	sketchybar --trigger brew_update
+	exit 0
+fi
