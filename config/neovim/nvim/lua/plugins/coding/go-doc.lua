2ba9d0626e7dc5e73f80a3f38f21610aaed99a04
Emre Armagan <emrearmagan.dev@gmail.com>
2025-11-02 03:06:49 +0100

    add go-docs

---
+++ config/neovim/nvim/lua/plugins/coding/go-doc.lua
@@ -0,0 +1,10 @@
+return {
+	"fredrikaverpil/godoc.nvim",
+	version = "*",
+	dependencies = {
+		{ "nvim-telescope/telescope.nvim" }, -- optional
+	},
+	build = "go install github.com/lotusirous/gostdsym/stdsym@latest", -- optional
+	cmd = { "GoDoc" }, -- optional
+	opts = {}, -- see further down below for configuration
+}
