a70703e892d04cbc0fef31631ce5bf1e2f588626
Emre Armagan <emrearmagan.dev@gmail.com>
2025-11-09 17:27:39 +0100

    update config

--- config/neovim/nvim/lua/plugins/ui/dropbar.lua
+++ config/neovim/nvim/lua/plugins/ui/dropbar.lua
@@ -1,9 +1,16 @@
 return {
 	"Bekaboo/dropbar.nvim",
 	config = function()
+		local sources = require("dropbar.sources")
 		-- Configuration goes here
 		require("dropbar").setup({
 			bar = {
+				-- show only file/folder path
+				sources = function(_, _)
+					return { sources.path }
+				end,
+				truncate = true, -- optional: truncate long paths
+
 				padding = {
 					left = 1,
 					right = 1,
