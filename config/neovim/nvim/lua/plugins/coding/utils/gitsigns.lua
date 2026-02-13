25402aff6f3e6e9df20e5f7fea163a615bad5980
Emre Armagan <emrearmagan.dev@gmail.com>
2026-02-13 17:13:47 +0100

    update gitsign

--- config/neovim/nvim/lua/plugins/coding/utils/gitsigns.lua
+++ config/neovim/nvim/lua/plugins/coding/utils/gitsigns.lua
@@ -4,15 +4,23 @@ return {
 	config = function()
 		require("gitsigns").setup({
 			signs = {
-				add = { text = "│" },
-				change = { text = "│" },
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged = {
+				add = { text = "┃" },
+				change = { text = "┃" },
 				delete = { text = "_" },
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 				untracked = { text = "┆" },
 			},
 			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
 			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 			watch_gitdir = {
@@ -24,10 +32,10 @@ return {
 			current_line_blame_opts = {
 				virt_text = true,
 				virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
-				delay = 500,
+				delay = 200,
 				ignore_whitespace = false,
 			},
-			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+			current_line_blame_formatter = "<author>, <author_time:%d-%m-%Y> - <summary>",
 			sign_priority = 6,
 			update_debounce = 100,
 			status_formatter = nil, -- Use default
