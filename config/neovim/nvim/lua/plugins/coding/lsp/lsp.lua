ea43507017126d3414922f7290aad639bc96919c
Emre Armagan <emrearmagan.dev@gmail.com>
2026-02-28 23:12:24 +0100

    add custom filetype to support yml

--- config/neovim/nvim/lua/plugins/coding/lsp/lsp.lua
+++ config/neovim/nvim/lua/plugins/coding/lsp/lsp.lua
@@ -47,6 +47,14 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			-- Fix filetypes for LSP warnings
+			vim.filetype.add({
+				extension = { yml = "yaml" },
+				pattern = {
+					["*.yml"] = "yaml",
+				},
+			})
+
 			-- Auto-populate quickfix with diagnostics
 			vim.api.nvim_create_autocmd("DiagnosticChanged", {
 				callback = function()
