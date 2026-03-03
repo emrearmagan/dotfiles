115af8bbb55d1dd93395d39f50a9408846028126
Emre Armagan <emrearmagan.dev@gmail.com>
2026-03-03 21:54:54 +0100

    Integrate godoc.nvim with Treesitter for improved parsing

--- config/neovim/nvim/lua/plugins/coding/utils/go-doc.lua
+++ config/neovim/nvim/lua/plugins/coding/utils/go-doc.lua
@@ -2,9 +2,53 @@ return {
 	"fredrikaverpil/godoc.nvim",
 	version = "*",
 	dependencies = {
-		{ "nvim-telescope/telescope.nvim" }, -- optional
+		{
+			"nvim-treesitter/nvim-treesitter",
+			branch = "main",
+			build = ":TSUpdate godoc go",
+			config = function()
+				require("nvim-treesitter.parsers").godoc = {
+					install_info = {
+						url = "https://github.com/fredrikaverpil/tree-sitter-godoc",
+						files = { "src/parser.c" },
+						branch = "main",
+					},
+					filetype = "godoc",
+				}
+
+				vim.treesitter.language.register("godoc", "godoc")
+			end,
+		},
+		"nvim-telescope/telescope.nvim",
 	},
-	build = "go install github.com/lotusirous/gostdsym/stdsym@latest", -- optional
-	cmd = { "GoDoc" }, -- optional
-	opts = {}, -- see further down below for configuration
+	build = "go install github.com/lotusirous/gostdsym/stdsym@latest",
+	cmd = { "GoDoc" },
+	opts = {
+		picker = {
+			type = "telescope",
+		},
+		adapters = {
+			{
+				name = "go",
+				opts = {
+					get_syntax_info = function()
+						return {
+							filetype = "godoc",
+							language = "godoc",
+						}
+					end,
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("godoc").setup(opts)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "godoc",
+			callback = function(ev)
+				pcall(vim.treesitter.start, ev.buf, "godoc")
+			end,
+		})
+	end,
 }
