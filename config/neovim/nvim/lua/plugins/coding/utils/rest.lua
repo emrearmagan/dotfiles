79770baedba94e0de8218587f822d3dd4042c0cb
Emre Armagan <emrearmagan.dev@gmail.com>
2026-03-14 12:58:31 +0100

    replace rest.nvim with kulala.nvim

--- config/neovim/nvim/lua/plugins/coding/utils/rest.lua
+++ config/neovim/nvim/lua/plugins/coding/utils/rest.lua
@@ -1,43 +1,84 @@
 return {
-	"rest-nvim/rest.nvim",
-	ft = "http",
-	rocks = false, -- Disable luarocks installation
+	"mistweaverco/kulala.nvim",
 	dependencies = {
-		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			opts.ensure_installed = opts.ensure_installed or {}
-			table.insert(opts.ensure_installed, "http")
-		end,
 	},
-	config = function()
-		require("rest-nvim").setup({
-			request = {
-				skip_ssl_verification = false,
-				hooks = {
-					encode_url = false,
-				},
+	ft = { "http", "rest" },
+	opts = {
+		-- global_keymaps = true,
+		-- global_keymaps_prefix = "<leader>Z",
+		-- kulala_keymaps_prefix = "",
+		vscode_rest_client_environmentvars = true, -- load from shell env vars
+		max_response_size = 500000,
+
+		ui = {
+			display_mode = "split", -- show response in split
+			split_direction = "vertical", -- right side split
+			winbar = true,
+			default_winbar_panes = { "body", "headers", "verbose", "help" },
+			report = {
+				headersHighlight = "Sky",
+				successHighlight = "Green",
+				errorHighlight = "Error",
 			},
-			response = {
-				hooks = {
-					decode_url = true,
-					format = true, -- pretty-print JSON / XML
-				},
+			win_opts = {
+				width = math.floor(vim.o.columns * 0.5),
 			},
-			ui = {
-				winbar = true,
-				keybinds = {
-					prev = "H",
-					next = "L",
-				},
+		},
+
+		contenttypes = {
+			["application/json"] = {
+				ft = "json",
+				formatter = vim.fn.executable("jq") == 1 and { "jq", "." } or nil,
 			},
-			cookies = {
-				enable = true,
+		},
+	},
+
+	config = function(_, opts)
+		require("kulala").setup(opts)
+
+		vim.filetype.add({
+			filename = {
+				[".http"] = "http",
+				[".rest"] = "rest",
 			},
-			env = {
-				enable = true,
-				pattern = ".*%.env.*",
+			extension = {
+				["http"] = "http",
+				["rest"] = "rest",
 			},
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "http", "rest" },
+			callback = function()
+				--  i dont know why there was no highlight for http filetypes, maybe treesitter was not started? this helps for now
+				local ft = vim.bo.filetype
+				local lang = vim.treesitter.language.get_lang(ft) or ft
+				local ok_parser, parser = pcall(vim.treesitter.get_parser, 0)
+				if ok_parser and parser and parser.lang and parser:lang() ~= lang then
+					pcall(vim.treesitter.stop, 0)
+				end
+				pcall(vim.treesitter.start, 0, lang)
+
+				--- Keymaps
+
+				local cfg = { buffer = true, silent = true }
+				vim.keymap.set({ "n", "v" }, "<leader>R", function()
+					require("kulala").run()
+				end, vim.tbl_extend("force", cfg, { desc = "Run request" }))
+
+				vim.keymap.set({ "n", "v" }, "<leader>I", function()
+					require("kulala").inspect()
+				end, vim.tbl_extend("force", cfg, { desc = "Inpect request" }))
+
+				vim.keymap.set("n", "<leader>L", function()
+					require("kulala").replay()
+				end, vim.tbl_extend("force", cfg, { desc = "Replay last request" }))
+
+				vim.keymap.set("n", "<leader>E", function()
+					require("kulala").set_selected_env()
+				end, vim.tbl_extend("force", cfg, { desc = "Select env" }))
+			end,
 		})
 	end,
 }
