b31f1c6b3e4f49c4e4bec3f40b7a971d8889bfca
Emre Armagan <emrearmagan.dev@gmail.com>
2026-04-09 01:32:08 +0200

    simplify cmdline

--- config/neovim/nvim/lua/plugins/ui/noice.lua
+++ config/neovim/nvim/lua/plugins/ui/noice.lua
@@ -8,7 +8,17 @@ return {
 		popupmenu = { enabled = false },
 		notify = { enabled = true },
 		messages = { enabled = true },
-		cmdline = { enabled = true, view = "cmdline" }, -- bottom style
+		cmdline = {
+			enabled = true,
+			view = "cmdline", -- bottom style
+			format = {
+				search_down = { kind = "search", pattern = "^/", icon = " /", lang = "regex" },
+				search_up = { kind = "search", pattern = "^%?", icon = " ?", lang = "regex" },
+				filter = { pattern = "^:%s*!", icon = " !", lang = "bash" },
+				lua = { pattern = "^:%s*lua%s+", icon = " lua", lang = "lua" },
+				help = { pattern = "^:%s*he?l?p?%s+", icon = " ?" },
+			},
+		},
 		lsp = {
 			progress = { enabled = true, view = "mini" },
 			message = { enabled = true },
