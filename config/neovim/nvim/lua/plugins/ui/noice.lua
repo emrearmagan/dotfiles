b2b4f71ee13b7ea06988647debd6b350fb8c5796
Emre Armagan <emrearmagan.dev@gmail.com>
2026-04-09 01:34:02 +0200

    add cmdline

--- config/neovim/nvim/lua/plugins/ui/noice.lua
+++ config/neovim/nvim/lua/plugins/ui/noice.lua
@@ -12,11 +12,12 @@ return {
 			enabled = true,
 			view = "cmdline", -- bottom style
 			format = {
-				search_down = { kind = "search", pattern = "^/", icon = " /", lang = "regex" },
-				search_up = { kind = "search", pattern = "^%?", icon = " ?", lang = "regex" },
-				filter = { pattern = "^:%s*!", icon = " !", lang = "bash" },
-				lua = { pattern = "^:%s*lua%s+", icon = " lua", lang = "lua" },
-				help = { pattern = "^:%s*he?l?p?%s+", icon = " ?" },
+				cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+				search_down = { kind = "search", pattern = "^/", icon = "/", lang = "regex" },
+				search_up = { kind = "search", pattern = "^%?", icon = "?", lang = "regex" },
+				filter = { pattern = "^:%s*!", icon = "!", lang = "bash" },
+				lua = { pattern = "^:%s*lua%s+", icon = "lua", lang = "lua" },
+				help = { pattern = "^:%s*he?l?p?%s+", icon = "help" },
 			},
 		},
 		lsp = {
